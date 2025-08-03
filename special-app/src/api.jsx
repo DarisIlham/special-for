c303c2a882abb1400184d1daacda882dd373039f
DarisIlham <darisilham32@gmail.com>
2025-08-03 11:37:11 +0700

    fix apijsx

--- special-app/src/api.jsx
+++ special-app/src/api.jsx
@@ -1,5 +1,5 @@
 // src/api.js
-const API_BASE_URL = 'http://localhost:5000/api'; // Adjust if your backend is hosted elsewhere
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;; // Adjust if your backend is hosted elsewhere
 
 export const fetchWishes = async () => {
   const response = await fetch(`${API_BASE_URL}/wishes`);
